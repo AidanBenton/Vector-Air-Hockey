63dd0dc2090348e0cd67a91798519dc3758cee18
AidanBenton <aidanbenton2005@gmail.com>
2021-12-10 23:44:02 -0500

    Added start menu, Finished Game

--- Vector Air Hockey/Form1.cs
+++ Vector Air Hockey/Form1.cs
@@ -29,7 +29,7 @@ namespace Vector_Air_Hockey
         Pen drawPen = new Pen(Color.White, 3);
         Pen greyPen = new Pen(Color.Gray, 3);
         //Game states
-        string gameState = "running"; 
+        string gameState = "start"; 
         //Physics variables/stopwatch
         bool collsion = false;
         Stopwatch MyWatch = new Stopwatch();
@@ -54,6 +54,8 @@ namespace Vector_Air_Hockey
         bool downArrow;
         bool leftArrow;
         bool rightArrow;
+        bool enter;
+        bool esc; 
         //player information
         int player1PosX;
         int player1PosY;
@@ -117,6 +119,12 @@ namespace Vector_Air_Hockey
                 case Keys.Right:
                     rightArrow = false;
                     break;
+                case Keys.Enter:
+                    enter = false;
+                    break;
+                case Keys.Escape:
+                    esc = false;
+                    break;
             }
         }
         private void Form1_KeyDown(object sender, KeyEventArgs e)
@@ -148,210 +156,271 @@ namespace Vector_Air_Hockey
                 case Keys.Right:
                     rightArrow = true;
                     break;
+                case Keys.Enter:
+                    enter = true;
+                    break;
+                case Keys.Escape:
+                    esc = true;
+                    break;
             }
         }
         private void gameTimer_Tick(object sender, EventArgs e)
         {
-
-
-
-            //ball stuck with walls 
-            if (position.X == 10)
+            if (gameState == "start")
             {
-                MyWatch.Start();
-            }
-            else
-            {
-                MyWatch.Stop();
-            }
-
-
-            if (wDown == true && player1PosY > 50)
-            {
-                player1PosY -= playerSpeedY;
-            }
-            if (sDown == true && player1PosY + 42 < 250)
-            {
-                player1PosY += playerSpeedY;
-            }
-            if (aDown == true && player1PosX > 10)
-            {
-                player1PosX -= playerSpeedX;
-            }
-            if (dDown == true && player1PosX < 237 - 40)
-            {
-                player1PosX += playerSpeedX;
-            }
-
-            if (upArrow == true && player2PosY - 2 > 250)
-            {
-                player2PosY -= playerSpeedY;
-            }
-            if (downArrow == true && player2PosY < 451 - 35)
-            {
-                player2PosY += playerSpeedY;
-            }
-            if (leftArrow == true && player2PosX > 10)
-            {
-                player2PosX -= playerSpeedX;
-            }
-            if (rightArrow == true && player2PosX < 237 - 40)
-            {
-                player2PosX += playerSpeedX;
-            }
-
-            //player one's distance to the ball
-            player1XDistance = Convert.ToSingle((player1PosX + 20) - (position.X + 10));
-            player1YDistance = Convert.ToSingle((player1PosY + 20) - (position.Y + 10));
-            player1Distance = Convert.ToSingle(Math.Sqrt(Math.Pow(player1XDistance, 2) + Math.Pow(player1YDistance, 2)));
-
-            //player two's distance to the ball
-            player2XDistance = Convert.ToSingle((player2PosX + 20) - (position.X + 10));
-            player2YDistance = Convert.ToSingle((player2PosY + 20) - (position.Y + 10));
-            player2Distance = Convert.ToSingle(Math.Sqrt(Math.Pow(player2XDistance, 2) + Math.Pow(player2YDistance, 2)));
-
-            if (player2Distance <= 30)
-            {
-                collisionCalculation(player2XDistance, player2YDistance, player2Distance);
-                //Fnet = FA x (mass x coefficiant of friction) 
-                xFnet = Fax;
-                // A = fnet * mass
-                xA = xFnet / 20;
-                XV1 = Convert.ToInt32(xA * time);
-
-                //Fnet = FA x (mass x coefficiant of friction) 
-                yFnet = Fay;
-                // A = fnet * mass
-                yA = yFnet / 20;
-                //V1 = V2 + A(t)
-                YV1 = Convert.ToInt32(yA * time);
-            }
-
-            //Angle and friction calculations
-            if (player1Distance <= 30)
-            {
-                collisionCalculation(player1XDistance, player1YDistance, player1Distance);
-                //Fnet = FA x (mass x coefficiant of friction) 
-                xFnet = Fax;
-                // A = fnet * mass
-                xA = xFnet / 20;
-                XV1 = Convert.ToInt32(xA * time);
-
-                //Fnet = FA x (mass x coefficiant of friction) 
-                yFnet = Fay;
-                // A = fnet * mass
-                yA = yFnet / 20;
-                //V1 = V2 + A(t)
-                YV1 = Convert.ToInt32(yA * time);
-            }
-            runTime++;
-            if (runTime % 8 == 0)
-            {
-                if (XV1 < 0)
+                //Start up mode
+                playerScoredLabel.Location = new System.Drawing.Point(65, 222);
+                playerScoredLabel.Text = $"Air Hockey \nPLAY: ENTER \nEXIT: ESC";
+                //Starts game
+                if (enter == true)
                 {
-                    XV1++;
+                    gameState = "running";
+                    playerScoredLabel.Text = $"";
+                    for (int i = 0; i < 500; i++)
+                    {
+                        backgroundLabelTop.Size = new System.Drawing.Size(250, 500);
+                        backgroundLabelTop.Size = new System.Drawing.Size(250, 500 - i);
+                        Refresh();
+
+                    }
+                    playerScoredLabel.Location = new System.Drawing.Point(24, 241);
                 }
-                else if (XV1 > 0)
+                //Closes program
+                else if (esc)
                 {
-                    XV1--;
-                }
-
-                if (YV1 < 0)
-                {
-                    YV1++;
-                }
-                else if (YV1 > 0)
-                {
-                    YV1--;
+                    this.Close(); 
                 }
             }
-
-            //all weight is represented in grams
-
-
-
-            //ball collsion with walls
-            if (position.X - 3 <= 10)
+            //used for goal scoring procedure, and game over procedure
+            if (gameState == "stalled")
             {
-                //collsion with left wall
-                if (XV1 != Math.Abs(XV1))
+                //restarts game
+                if (enter == true)
                 {
-                    XV1 = XV1 * -1;
-                    position.X = 11;
+                    pOneScore = 0;
+                    pTwoScore = 0;
+                    XV1 = 0;
+                    YV1 = 0;
+                    position.X = 125 - 10;
+                    position.Y = 250 - 10;
+
+                    player1PosX = 125 - 20;
+                    player1PosY = 80;
+
+                    player2PosX = 125 - 20;
+                    player2PosY = 431 - 40;
+                    backgroundLabelTop.Visible = true;
+                    gameState = "running"; 
+                    for (int i = 0; i < 500; i++)
+                    {
+                        backgroundLabelTop.Size = new System.Drawing.Size(250, 500 - i);
+                        Refresh();
+                    }
+                    gameTimer.Enabled = true;
+                }
+                //Closes game
+                else if (esc == true)
+                {
+                    this.Close();
                 }
             }
-            if (position.X + 10 >= 223)
+            //game running procedure
+            else if (gameState == "running")
             {
-                //collsion wil right wall
-                if (XV1 == Math.Abs(XV1))
+                //player one input resolution 
+                if (wDown == true && player1PosY > 50)
                 {
-                    XV1 *= -1;
-                    position.X = 223 - 20;
+                    player1PosY -= playerSpeedY;
                 }
-            }
-            if (position.Y <= 50 && position.X <= 83 || position.Y <= 50 && position.X + 20 >= 162)
-            {
-                //collision with top wall
-                if (YV1 != Math.Abs(YV1))
+                if (sDown == true && player1PosY + 42 < 250)
                 {
-                    YV1 *= -1;
-                    position.Y = 50;
+                    player1PosY += playerSpeedY;
                 }
-            }
-            if (position.Y + 20 >= 451 && position.X + 20 <= 83 || position.Y + 20 >= 451 && position.X + 20 >= 162)
-            {
-                //collision bottom wall
-                if (YV1 == Math.Abs(YV1))
+                if (aDown == true && player1PosX > 10)
                 {
-                    YV1 *= -1;
-                    position.Y = 451 - 20;
+                    player1PosX -= playerSpeedX;
                 }
+                if (dDown == true && player1PosX < 237 - 40)
+                {
+                    player1PosX += playerSpeedX;
+                }
+
+                //player one input resolution 
+                if (upArrow == true && player2PosY - 2 > 250)
+                {
+                    player2PosY -= playerSpeedY;
+                }
+                if (downArrow == true && player2PosY < 451 - 35)
+                {
+                    player2PosY += playerSpeedY;
+                }
+                if (leftArrow == true && player2PosX > 10)
+                {
+                    player2PosX -= playerSpeedX;
+                }
+                if (rightArrow == true && player2PosX < 237 - 40)
+                {
+                    player2PosX += playerSpeedX;
+                }
+
+                //player one's distance to the ball
+                player1XDistance = Convert.ToSingle((player1PosX + 20) - (position.X + 10));
+                player1YDistance = Convert.ToSingle((player1PosY + 20) - (position.Y + 10));
+                player1Distance = Convert.ToSingle(Math.Sqrt(Math.Pow(player1XDistance, 2) + Math.Pow(player1YDistance, 2)));
+
+                //player two's distance to the ball
+                player2XDistance = Convert.ToSingle((player2PosX + 20) - (position.X + 10));
+                player2YDistance = Convert.ToSingle((player2PosY + 20) - (position.Y + 10));
+                player2Distance = Convert.ToSingle(Math.Sqrt(Math.Pow(player2XDistance, 2) + Math.Pow(player2YDistance, 2)));
+
+                //player one collision and angle calculations
+                if (player2Distance <= 30)
+                {
+                    //calculates angle
+                    collisionCalculation(player2XDistance, player2YDistance, player2Distance);
+                    //Fnet = FA x (mass x coefficiant of friction) 
+                    xFnet = Fax;
+                    // A = fnet * mass
+                    xA = xFnet / 20;
+                    XV1 = Convert.ToInt32(xA * time);
+
+                    //Fnet = FA x (mass x coefficiant of friction) 
+                    yFnet = Fay;
+                    // A = fnet * mass
+                    yA = yFnet / 20;
+                    //V1 = V2 + A(t)
+                    YV1 = Convert.ToInt32(yA * time);
+                }
+
+                //player two collision and angle calculations
+                if (player1Distance <= 30)
+                {
+                    //calculates angle
+                    collisionCalculation(player1XDistance, player1YDistance, player1Distance);
+                    //Fnet = FA x (mass x coefficiant of friction) 
+                    xFnet = Fax;
+                    // A = fnet * mass
+                    xA = xFnet / 20;
+                    XV1 = Convert.ToInt32(xA * time);
+
+                    //Fnet = FA x (mass x coefficiant of friction) 
+                    yFnet = Fay;
+                    // A = fnet * mass
+                    yA = yFnet / 20;
+                    //V1 = V2 + A(t)
+                    YV1 = Convert.ToInt32(yA * time);
+                }
+
+                //simulates friction
+                runTime++;
+                //checks if runtime/8 has a remainder of 0
+                if (runTime % 8 == 0)
+                {
+                    if (XV1 < 0)
+                    {
+                        XV1++;
+                    }
+                    else if (XV1 > 0)
+                    {
+                        XV1--;
+                    }
+
+                    if (YV1 < 0)
+                    {
+                        YV1++;
+                    }
+                    else if (YV1 > 0)
+                    {
+                        YV1--;
+                    }
+                }
+
+                //all weight is represented in grams
+                //ball collsion with walls
+                if (position.X - 3 <= 10)
+                {
+                    //collsion with left wall
+                    if (XV1 != Math.Abs(XV1))
+                    {
+                        XV1 = XV1 * -1;
+                        position.X = 11;
+                    }
+                }
+                if (position.X + 10 >= 223)
+                {
+                    //collsion wil right wall
+                    if (XV1 == Math.Abs(XV1))
+                    {
+                        XV1 *= -1;
+                        position.X = 223 - 20;
+                    }
+                }
+                if (position.Y <= 50 && position.X <= 83 || position.Y <= 50 && position.X + 20 >= 162)
+                {
+                    //collision with top wall
+                    if (YV1 != Math.Abs(YV1))
+                    {
+                        YV1 *= -1;
+                        position.Y = 50;
+                    }
+                }
+                if (position.Y + 20 >= 451 && position.X + 20 <= 83 || position.Y + 20 >= 451 && position.X + 20 >= 162)
+                {
+                    //collision bottom wall
+                    if (YV1 == Math.Abs(YV1))
+                    {
+                        YV1 *= -1;
+                        position.Y = 451 - 20;
+                    }
+                }
+
+                //Vector math
+                velocity = new Vector(XV1, YV1);
+                position += velocity;
+
+                //Goal collision calculation followed by goal scored procedure 
+                if (position.Y + 10 < goalOne.Y)
+                {
+                    XV1 = 0;
+                    YV1 = 0;
+                    pTwoScore++;
+                    playerGoalSleep("TWO", pTwoScore);
+                    position.X = 125 - 10;
+                    position.Y = 250 - 10;
+
+                    player1PosX = 125 - 20;
+                    player1PosY = 80;
+
+                    player2PosX = 125 - 20;
+                    player2PosY = 431 - 40;
+                }
+                //Goal collision calculation followed by goal scored procedure 
+                if (position.Y > goalTwo.Y)
+                {
+                    XV1 = 0;
+                    YV1 = 0;
+                    pOneScore++;
+                    playerGoalSleep("ONE", pOneScore);
+                    position.X = 125 - 10;
+                    position.Y = 250 - 10;
+
+                    player1PosX = 125 - 20;
+                    player1PosY = 80;
+
+                    player2PosX = 125 - 20;
+                    player2PosY = 431 - 40;
+                }
+                Refresh();
             }
-
-
-            velocity = new Vector(XV1, YV1);
-            position += velocity;
-            
-            if (position.Y + 10 < goalOne.Y)
-            {
-                XV1 = 0;
-                YV1 = 0;
-                pTwoScore++;
-                playerGoalSleep("TWO", pTwoScore);
-                position.X = 125 - 10;
-                position.Y = 250 - 10;
-
-                player1PosX = 125 - 20;
-                player1PosY = 80;
-
-                player2PosX = 125 - 20;
-                player2PosY = 431 - 40;
-            }
-            if (position.Y > goalTwo.Y)
-            {
-                XV1 = 0;
-                YV1 = 0;
-                pOneScore++;
-                playerGoalSleep("ONE", pOneScore);
-                position.X = 125 - 10;
-                position.Y = 250 - 10;
-
-                player1PosX = 125 - 20;
-                player1PosY = 80;
-
-                player2PosX = 125 - 20;
-                player2PosY = 431 - 40;
-            }
-
-
-            Refresh();
         }
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
+            //Game state check
             if (gameState == "stalled")
             {
                 e.Graphics.Clear(Color.Black);
             }
+            //Game state check
             else if (gameState == "running")
             {
                 e.Graphics.DrawString($"{pOneScore}", drawFont, drawBrush, 218, 228);
@@ -392,7 +461,7 @@ namespace Vector_Air_Hockey
 
         public void collisionCalculation(float xDistance, float yDistance, float playerDistance)
         {
-
+            //collision calculations
             // Cos = Cos-1(Adj/Hyp) 
             Cos = Math.Acos((xDistance / playerDistance));
             // Fax = FA * Cos
@@ -413,11 +482,13 @@ namespace Vector_Air_Hockey
             gameTimer.Enabled = false;
                 for (int i = 0; i < 500; i++)
                 {
+                //creates wall sinking down animation
                     backgroundLabelTop.Size = new System.Drawing.Size(500, i);
                     Refresh();
                 }
            
             backgroundLabelTop.Visible = false;
+            //changes game state
             gameState = "stalled";
             //checks if player has won
             if (playerScore == 3)
@@ -429,6 +500,7 @@ namespace Vector_Air_Hockey
             {
                 //displays various information on screen
                 g.FillRectangle(blackBrush, 0, 0, 250, 500);
+                playerScoredLabel.Location = new System.Drawing.Point(24, 241);
                 await Task.Delay(750);
                 playerScoredLabel.Text = $"PLAYER {playerGoal} SCORED";
                 await Task.Delay(750);
@@ -439,6 +511,7 @@ namespace Vector_Air_Hockey
                 playerScoredLabel.Text = $"";
                 gameState = "running";
                 backgroundLabelTop.Visible = true;
+                //creates wall rising up animation
                 for (int i = 0; i < 500; i++)
                 {
                     backgroundLabelTop.Size = new System.Drawing.Size(250, 500 - i);
@@ -450,6 +523,7 @@ namespace Vector_Air_Hockey
 
         public async Task gameOver(string whoWon)
         {
+            //Game over procedure
             Graphics g = this.CreateGraphics();
             Font drawFont = new Font("MS PGothic", 14, FontStyle.Regular);
             SolidBrush drawBrush = new SolidBrush(Color.White);
@@ -464,7 +538,8 @@ namespace Vector_Air_Hockey
             await Task.Delay(750);
             playerScoredLabel.Text = $"";
             await Task.Delay(750);
-            g.DrawString($"PLAY AGAIN: ENTER \nEXIT: ESC", drawFont, drawBrush, 24, 241);  
+            g.DrawString($"PLAY AGAIN: ENTER \nEXIT: ESC", drawFont, drawBrush, 24, 241);
+            gameTimer.Enabled = true;
         }
     }
 }
